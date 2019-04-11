ee920b497541bdcc4e641e4ad12f700707f8ab1a
Mike Nortje <mdnortje@gmail.com>
2019-04-11 15:21:46 +0200

    Algorithm i) Simple word count
    - added stop_words.txt

--- word_count.py
+++ word_count.py
@@ -1,21 +1,46 @@
 from mrjob.job import MRJob
+from mrjob.step import MRStep
 import re
 
 WORD_RE = re.compile(r"[\w']+")
 
-
 class MRWordFreqCount(MRJob):
+    FILES = ['text/stop_words.txt']
 
-    def mapper(self, _, line):
+    def steps(self):
+        return [
+            MRStep(
+                mapper_init=self.mapper_init,
+                mapper=self.mapper_get_words,
+                combiner=self.combiner,
+                reducer=self.reducer,
+            ),
+            MRStep(
+                reducer=self.reducer_sort_counts,
+            ),
+        ]
+
+    def mapper_init(self):
+        with open('stop_words.txt') as f:
+            self.stop_words = set(line.strip() for line in f)
+
+    def mapper_get_words(self, _, line):
         for word in WORD_RE.findall(line):
-            yield (word.lower(), 1)
+            word = word.lower()
+            if word not in self.stop_words:
+                yield (word.lower(), 1)
 
     def combiner(self, word, counts):
         yield (word, sum(counts))
 
-    def reducer(self, word, counts):
-        yield (word, sum(counts))
+    def reducer(self, key, values):
+        yield None, (sum(values), key)
 
+    def reducer_sort_counts(self, _, word_counts):
+        for count, key in sorted(word_counts, reverse=False):
+            yield ('%d' % int(count), key)
+        
 
 if __name__ == '__main__':
-     MRWordFreqCount.run()
+    MRWordFreqCount.run()
+    
