bd2eea2d7d0f41746e05a485cd3cd191668f6ae3
Tyson <tyson.cross@gmail.com>
2019-04-13 22:19:26 +0200

    Inverse indexing algorithm working

--- WordLineFreq.py
+++ WordLineFreq.py
@@ -1,4 +1,5 @@
 from mrjob.job import MRJob
+from mrjob.protocol import JSONValueProtocol
 from mrjob.step import MRStep
 import sys
 import re
@@ -8,29 +9,27 @@ WORD_RE = re.compile(r"[\w']+")
 class WordLineFreq(MRJob):
     FILES = ['stop_words.txt']
     SORT_VALUES = True
-
+    
     def configure_args(self):
         super(WordLineFreq, self).configure_args()
-        self.add_file_arg(
-            '--stop-words',
-            metavar='STOP_WORDS_FILE',
-            dest='stop_words',
-            type=str,
-            default='stop_words.txt',
-            help='Input stop words text file')
-        self.add_passthru_arg(
-            '--limit',
-            metavar='K',
-            dest='K',
-            type=int,
-            default=10,
-            help='Number of words to return')
+        self.add_file_arg(  '--stop-words',
+                            metavar='STOP_WORDS_FILE',
+                            dest='stop_words',
+                            type=str,
+                            default='stop_words.txt',
+                            help='Input stop words text file')
+        self.add_passthru_arg(  '--limit',
+                                metavar='K',
+                                dest='K',
+                                type=int,
+                                default=50,
+                                help='Number of words to return')
 
     def steps(self):
         return [
             MRStep(
                 mapper_init=self.mapper_init,
-                mapper=self.mapper_get_words,
+                mapper_raw=self.mapper_raw,
                 reducer=self.reducer,
             )
         ]
@@ -40,20 +39,19 @@ class WordLineFreq(MRJob):
         with open(self.options.stop_words) as f:
             self.stop_words = set(line.strip() for line in f)
 
-    def mapper_get_words(self, _, line):
-        # get line number here?
-        for word in WORD_RE.findall(line):
-            word = word.lower()
-            if word not in self.stop_words:
-                # sys.stderr.write("MAPPER INPUT: ({0})\n".format(line_list))  # debug
-                yield (word.lower(), line)
-                # line_num = [v for v in line]
-                # line_num = line.index(line)
-                    
+    def mapper_raw(self, path, _):
+            with open(path, 'r') as f:
+                for num, line in enumerate(f):
+                    if num>= self.options.K: break
+                    for word in line.rstrip().split():
+                        word = word.lower()
+                        if word not in self.stop_words:
+                            yield (word.lower(), num) # optionally, can pass [num, line.rstrip()]
+
     def reducer(self, key, values):
-        # sys.stderr.write("REDUCER INPUT: ({0},{1})\n".format(key,values)) # debug
-        line = [v for v in values] # generator -> list
-        yield (key, line)
+        line = [v for v in values]
+        # sys.stderr.write("output: ({0})\n".format(line))
+        yield (key, ','.join( str(a) for a in line ))
 
 if __name__ == '__main__':
     WordLineFreq.run()
--- line_numbers.py
+++ line_numbers.py
@@ -1,6 +1,16 @@
-filepath = 'text/iliad.txt'  
+# import json
+
+filepath = 'text/simple.txt'  
 enumerated_txt = {}
 with open(filepath) as fp:
     for cnt, line in enumerate(fp):
-       enumerated_txt[cnt] = line
-    print(enumerated_txt[1])
+        enumerated_txt[cnt] = line.rstrip()
+        # num,words = cnt+1,line.split()
+        # print(line.rstrip())
+
+print(enumerated_txt)
+# fileout = "dict.json"
+# json = json.dumps(enumerated_txt)
+# f = open(fileout,"w")
+# f.write(json)
+# f.close()
